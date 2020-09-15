b2f87d8dfd9d4e4de3824ac1281a3f6e1ea21430
Michael Huo <5288085+michaelhuo@users.noreply.github.com>
2020-09-14 21:16:26 -0700

    added sort.py

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("Hello World")
+import collections
+
+N = 5
+dist = collections.defaultdict(lambda: N * N)
+print(len(dict))
+
+
+print(dict)
+# for x in range(N):
+#     dict[ 1 << x, x ] = 0
+# print(dict)
